63f9dcd4ad8958307fc74d177d2c0110f61e601a
mc12345678 <mc12345678@users.noreply.github.com>
2017-06-18 22:25:48 -0400

    Simple text move/alignment.

--- 1_Installer_Files/includes/modules/pages/advanced_search/jscript_zen_colorbox.php
+++ 1_Installer_Files/includes/modules/pages/advanced_search/jscript_zen_colorbox.php
@@ -10,7 +10,7 @@
  */
 
 if (ZEN_COLORBOX_STATUS == 'true') {
-	require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'zen_colorbox/jquery_colorbox.php');
+  require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'zen_colorbox/jquery_colorbox.php');
 ?>
 
 <script language="javascript" type="text/javascript">
