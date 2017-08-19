8393903efc2628601a1ba9b2393f8c6687beeb06
mc12345678 <mc12345678@users.noreply.github.com>
2017-08-19 18:28:51 -0400

    Add installer with notes/documentation updating version.

---
+++ 1_Installer_Files/YOUR_ADMIN/includes/installers/zen_colorbox/2_1_1.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Zen Colorbox
+ *
+ * @copyright 2013 C Jones
+ * @copyright Copyright 2003-2017 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: mc12345678 thanks to bislewl 6/9/2015
+ */
+/*
+  V2.1.1, What changed:
+  
+  - Updated installer/updater to exclude operation if the logoff was selected.
+  - Relocated code that was disabling a properly formatted/named Image Handler 4 image to allow IH4 and
+  zen colorbox to continue to operate together.
+  - Completed the incorporation of the code template code to minimize code rewriting/retyping. This also
+  removed a javascript error that was generated on the checkout_payment page.
+  - Incorporate ZC 1.5.5 code into the includes/modules/YOUR_TEMPLATE/additional_images.php file including
+  notifiers and the comments at the top of the file.
+  - Removed the obsolete language="javascript" parameters from the HTML <script> tags.
+  
+*/
+
+ // END OF VERSION 1.5.x and 1.3.X INSTALL
